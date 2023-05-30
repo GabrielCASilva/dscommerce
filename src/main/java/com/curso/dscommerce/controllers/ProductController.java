4392599702c4cb7c7f6709bf7a1818d55dc32297
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-05-30 08:48:10 -0300

    create ProductRepository and inject into ProductController

--- src/main/java/com/curso/dscommerce/controllers/ProductController.java
+++ src/main/java/com/curso/dscommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.curso.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.dscommerce.entities.Product;
+import com.curso.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+	@Autowired
+	private ProductRepository repository;
+
 	@GetMapping
-	public String teste() {
-		return "Ola mundo!";
+	public String test() {
+		Optional<Product> result = repository.findById(1L);
+		Product product = result.get();
+		return product.getName();
 	}
 }
