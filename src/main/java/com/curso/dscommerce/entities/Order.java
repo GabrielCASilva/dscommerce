3d29c4171a4cc4befd3d9d3740069fc46d0573de
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-05-29 22:29:18 -0300

    relationship between Product, Order and OrderItem

--- src/main/java/com/curso/dscommerce/entities/Order.java
+++ src/main/java/com/curso/dscommerce/entities/Order.java
@@ -1,7 +1,10 @@
 package com.curso.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -32,6 +36,9 @@ public class Order {
 
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 	}
@@ -72,6 +79,14 @@ public class Order {
 
 	public Payment getPayment() {
 		return payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public List<Product> getProducts() {
+		return items.stream().map(x -> x.getProduct()).toList();
 	}
 
 	@Override
