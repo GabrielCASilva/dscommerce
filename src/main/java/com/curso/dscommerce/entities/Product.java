b75596f362238c27731080680887660c353cfe9b
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-05-29 22:10:55 -0300

    relationship between Category and Product

--- src/main/java/com/curso/dscommerce/entities/Product.java
+++ src/main/java/com/curso/dscommerce/entities/Product.java
@@ -1,12 +1,17 @@
 package com.curso.dscommerce.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +27,10 @@ public class Product {
 	private String description;
 	private Double price;
 	private String imgUrl;
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
 	}
