7e09faa290ad332631660444ad2384e62855095c
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-05-29 21:34:37 -0300

    user equals hashCode

--- src/main/java/com/curso/dscommerce/entities/User.java
+++ src/main/java/com/curso/dscommerce/entities/User.java
@@ -1,6 +1,7 @@
 package com.curso.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -82,6 +83,23 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
