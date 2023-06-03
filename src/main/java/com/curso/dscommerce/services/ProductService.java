6bc034977c4aef8b4076100db1e255cc71bdaed6
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-06-03 11:40:34 -0300

    GET products page

--- src/main/java/com/curso/dscommerce/services/ProductService.java
+++ src/main/java/com/curso/dscommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.curso.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,11 @@ public class ProductService {
 		Optional<Product> result = repository.findById(id);
 		Product product = result.get();
 		return new ProductDTO(product);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
 	}
 }
